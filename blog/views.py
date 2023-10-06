21621d1bb5f15ce7d93df84febdd7a2a0007aee3
Purnendu Talukder <ptsourav21@gmail.com>
2023-10-06 21:26:03 +0600

    Adding dummy data

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,24 @@
+from datetime import date
 from django.shortcuts import render
+
+posts =[
+    {
+        "slug":"hike-in-the-mountains",
+        "image": "Purnendu.png",
+        "author": "Purnendu",
+        "date": date(2021, 7, 21),
+        "title": "Mountain Hiking",
+        "excerpt": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, omnis nesciunt. Tenetur eius fugiat, culpa recusandae provident placeat in laborum.",
+        "content": """
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, omnis nesciunt. Tenetur eius fugiat, culpa recusandae provident placeat in laborum.
+
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, omnis nesciunt. Tenetur eius fugiat, culpa recusandae provident placeat in laborum.
+
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, omnis nesciunt. Tenetur eius fugiat, culpa recusandae provident placeat in laborum.
+"""
+
+    }
+]
 
 # Create your views here.
 def starting_page(request):
